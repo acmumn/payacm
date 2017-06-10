a449685ea7de5f01f8ff946668516927b3f0e670
Nathaniel Ringo <remexre@gmail.com>
2017-06-10 00:04:54 -0500

    Better error reporting, adds reason field.

--- static/main.js
+++ static/main.js
@@ -8,15 +8,34 @@ $(() => {
 
 	$("#amount").on("input", onAmountChange);
 	$("#pay").click(onPay);
-	if(window.location.hash) {
-		$("#amount").val(window.location.hash.slice(1));
+	const uri = URI(window.location.href);
+	const query = uri.query(true);
+	if(query.amount) {
+		$("#amount").val(query.amount);
+		$("#amount").attr("disabled", true);
 		onAmountChange();
 	}
+	if(query.reason) {
+		$("#reason").val(query.reason);
+		$("#reason").attr("disabled", true);
+	}
 
+	function addCard() {
+		return $("<div>")
+			.addClass("jumbotron")
+			.appendTo("main")
+			.hide()
+			.slideDown();
+	}
+	function getReason() {
+		return $("#reason").val();
+	}
 	function onAmountChange() {
 		const value = $("#amount").val();
 		if(value.match(/^[0-9]+(\.[0-9]{2})?$/)) {
 			amount = parseInt(value.replace(".", ""));
+			if(value.match(/^[0-9]+$/))
+				amount *= 100;
 
 			$("#amount-group").addClass("has-success");
 			$("#amount-group").removeClass("has-error");
@@ -30,15 +49,17 @@ $(() => {
 	function onPay(e) {
 		handler.open({
 			name: "ACM UMN",
-			description: "payacm",
+			description: getReason() || "payacmumn",
 			amount: amount,
 		});
 		e.preventDefault();
 	}
 	function onSubmit(token) {
+		$("#pay-form").slideUp();
 		const body = {
 			amount: amount,
 			email: token.email,
+			reason: getReason() || "",
 			token: token.id,
 		};
 		fetch("/", {
@@ -49,13 +70,20 @@ $(() => {
 				if(res.ok)
 					return json;
 				else
-					throw json;
+					throw { code: res.status + " " + res.statusText, contents: json };
 			});
 		}).then(res => {
 			alert("Success!");
 		}).catch(err => {
+			addCard()
+				.append($("<h2>")
+					.addClass("text-danger")
+					.text("An Error Occurred"))
+				.append($("<p>")
+					.html("Contact <a href='mailto:acm@umn.edu'>acm@umn.edu</a> with the details of this error."))
+				.append($("<pre>")
+					.append($("<code>").text(JSON.stringify(err, null, "\t"))));
 			console.error(err);
-			alert("An error occurred.");
 		});
 	}
 });
